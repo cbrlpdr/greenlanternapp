46b9be160e85f20fc25397c53481637ea0e7ae9c
cbrlpdr <cbrl.pdr@gmail.com>
2025-08-19 17:27:09 -0400

    feat: added green lantern feature

--- App.tsx
+++ App.tsx
@@ -1,19 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import onImg from "./assets/symbol-on.png";
+import offImg from "./assets/symbol-off.png";
+import { useState } from 'react';
 
 export default function App() {
+  const [isOn, setIsOn] = useState(false);
+
+  const handleLanternPress = () => {
+    setIsOn(!isOn);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View style={isOn?styles.containerOn:styles.containerOff}>
+      <TouchableOpacity onPress={handleLanternPress}>
+      <Image source={isOn? onImg : offImg} style={{ width: 180, height: 180 }} />
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  containerOn: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#020',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  containerOff: {
+    flex: 1,
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
